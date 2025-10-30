e01ba4a8dc2e5f247545581e74176272ce260051
Erik Rose <erik.w.rose@gmail.com>
2025-10-29 17:04:30 -0700

    feat: Updates to sewer workflow.

--- un_update.py
+++ un_update.py
@@ -6,22 +6,23 @@ from dateutil.relativedelta import relativedelta
 import sys
 
 sys.path.append(r"d:\repos")
-from un_update import Cleanout
+import un_update
 
 sys.path.append(r"c:\users\erose\repos\scripts")
-from helpers import created, get_created, field_map, select_within
+from helpers import created
 
 # configure environment in arcpy
 arcpy.env.parallelProcessingFactor = "100%"  # try to be parallel
 arcpy.env.overwriteOutput = True  # overwrite GDB
 
-LOG_FILE = "P:/un_update.log"
+# LOG_FILE = "P:/un_update.log"
 
 # format log messages to include time before message
 logging.basicConfig(
     format="%(asctime)s %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
-    filename=LOG_FILE,
+    stream=sys.stdout,
+    # filename=LOG_FILE,
     level=logging.INFO,
 )
 
@@ -65,79 +66,29 @@ print(f"Feature count: {count}")
 now = datetime.now()
 ago = now - relativedelta(months=6)
 
-
-new_clean_outs = "new_clean_outs"
-new_ids = created(clean_outs, "Sewer Device", "AssetID", "assetid")
-get_created(clean_outs, "Sewer Device", new_clean_outs)
-
-
-def clean_out_fms():
-    fms = arcpy.FieldMappings()
-    field_map(fms, new_clean_outs, "ACCESSDIAM", "diameter")
-    field_map(fms, new_clean_outs, "FACILITYID", "assetid")
-    field_map(fms, new_clean_outs, "NOTES", "notes")
-    field_map(fms, new_clean_outs, "HistoricID", "historic_id")
-    field_map(fms, new_clean_outs, "RIM", "rim")
-    field_map(fms, new_clean_outs, "OUT", "out")
-    field_map(fms, new_clean_outs, "AssetID", "facility_id")
-    return fms
-
-
-def update_cleanout_fields(new, old, new_id, old_id, ids, workspace):
-    new_clause = select_within(new_id, ids)
-    old_clause = select_within(old_id, ids)
-    with arcpy.da.SearchCursor(
-        new,
-        [
-            new_id,
-            "piCleanoutTypes",
-            "AssetStatus_2019",
-            "INSTALLDATE",
-            "AssetOwner_2019",
-            "piPipeMaterial",
-            "DataSource",
-            "SurveyPtNum",
-            "SurveyDate",
-        ],
-        new_clause,
-    ) as cursor:
-        for row in cursor:
-            print(row[0])
-            with arcpy.da.Editor(workspace):
-                with arcpy.da.UpdateCursor(
-                    old,
-                    [
-                        old_id,
-                        "assettype",
-                        "lifecyclestatus",
-                        "installdate",
-                        "ownedby",
-                        "material",
-                        "spatialsource",
-                        "constructionstatus",
-                        "survey_pt",
-                        "survey_date",
-                    ],
-                    old_clause,
-                ) as update_cursor:
-                    for j in update_cursor:
-                        print(j[0])
+# un_update.sewer.Cleanout.update_created(clean_outs, branch)  # type: ignore
+# un_update.sewer.Valve.update_created(valves, branch)  # type: ignore
+# un_update.sewer.Manhole.update_created(manholes, branch)  # type: ignore
+# un_update.sewer.Fitting.update_created(fittings, branch)  # type: ignore
+# un_update.sewer.GravityMain.update_created(mains, branch)  # type: ignore
+un_update.sewer.ForceMain.update_created(force, branch)  # type: ignore
 
 
 fc = arcpy.Describe(branch).catalogPath
-print("arcpy.Describe(branch).catalogPath = " + fc)
+print(f"arcpy.Describe(branch).catalogPath = {fc}")
 # Get just the .sde file (before the backslash)
 workspace = fc.split("\\")[0]
+print(f"workspace = {workspace}")
 
-cleanout = Cleanout(new_clean_outs)  # type: ignore
 
-arcpy.management.Append(
-    new_clean_outs, "Sewer Device", "NO_TEST", cleanout.field_mappings(), "Cleanout"
-)
-arcpy.management.AddJoin("Sewer Device", "assetid", new_clean_outs, "AssetID")
-clause = select_within("L1Sewer_Device.assetid", new_ids)
-arcpy.management.SelectLayerByAttribute("Sewer Device", "NEW_SELECTION", clause)
-cleanout.update(workspace)
+# arcpy.management.Append(
+#     new_clean_outs, "Sewer Device", "NO_TEST", cleanout.field_mappings(), "Cleanout"
+# )
+# arcpy.management.AddJoin("Sewer Device", "assetid", new_clean_outs, "AssetID")
+# clause = select_within("L1Sewer_Device.assetid", new_ids)
+# arcpy.management.SelectLayerByAttribute("Sewer Device", "NEW_SELECTION", clause)
+# cleanout.update(workspace)
+# arcpy.management.RemoveJoin("Sewer Device")
 
 
 # 2. Check if there's a selection
